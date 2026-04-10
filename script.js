5b630a04ff43b9051212bcf34b97eeaccdb676ad
gabrielramossalles-dot <gabriel.ramos.salles@escola.pr.gov.br>
2026-04-10 11:32:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Garante que o hover seja detectado de forma limpa
+document.querySelectorAll('.menu-item').forEach(item => {
+    item.addEventListener('mouseenter', () => {
+        // Você pode adicionar sons de "click" aqui se quiser estilo editor!
+    });
+});
